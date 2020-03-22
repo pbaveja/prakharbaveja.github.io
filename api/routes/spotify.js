21ef80a1234d6f27202d1f54e28c7c815186f88f
Prakhar Baveja <prakhar.baveja@gmail.com>
2020-03-22 23:11:11 +0530

    redis caching added

--- api/routes/spotify.js
+++ api/routes/spotify.js
@@ -1,23 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var spotifyApi = require('../lib/spotify');
+var redisClient = require('../lib/redis');
 
+// Get tracks in a playlist
 router.get('/playlists/:id', function(req, res, next) {
-	// Get tracks in a playlist
-	// res.json({ name: 'my Playlist Name' });
-	spotifyApi
-	  .getPlaylist(req.params.id)
-	  .then(
-	    function(data) {
-	    	// console.log('The playlist contains these tracks', data.body);
-	  		res.json(data.body);
-	    },
-	    function(err) {
-	    	console.log('Something went wrong!', err);
-			res.status(500).json({
-				error: err
-			});
-	    }
-	);
+	redisClient.hget('playlists', req.params.id, function(err, result) {
+		if (err) {
+			console.error(err);
+		} else {
+			if (result == null) {
+				spotifyApi
+				  .getPlaylist(req.params.id)
+				  .then(
+				    function(data) {
+				    	// console.log('The playlist contains these tracks', data.body);
+				  		redisClient.hset('playlists', req.params.id, JSON.stringify(data.body));
+				  		redisClient.expire('playlists', 60*60*24*2) // Two days expiry
+				  		res.json(data.body);
+				    },
+				    function(err) {
+				    	console.log('Something went wrong!', err);
+						res.status(500).json({
+							error: err
+						});
+				    }
+				);
+			} else {
+				res.json(JSON.parse(result));
+			}
+		}
+	});
 });
 module.exports = router;
