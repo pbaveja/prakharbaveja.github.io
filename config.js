652edb8239a416a1c94e15a4c28fe827ff38b58c
Prakhar Baveja <prakhar.baveja@gmail.com>
2021-07-05 00:30:45 +0530

    first commit with gatsbyjs blog with projects

---
+++ config.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+  url: 'https://prakharbaveja.com',
+  pathPrefix: '/',
+  title: 'Prakhar Baveja -  Full Stack Engineer',
+  subtitle: 'My corner on the web.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Projects',
+      path: '/'
+    },
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'Spotify x Me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Prakhar Baveja',
+    photo: '/photo.jpg',
+    bio: 'Welcome to my corner on the web. Here you can find some of my work with the technologies used to create them. Be it a restful API or an elegant box-shadow, I can help you craft it!',
+    contacts: {
+      email: 'prakhar.baveja@gmail.com',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: 'pbaveja',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'prakhar-baveja-244907106',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: 'prakhar.baveja',
+    }
+  }
+};
